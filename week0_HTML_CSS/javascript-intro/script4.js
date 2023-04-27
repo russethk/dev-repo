cda5c6fb592f37978f599807b8a73cabaa3affbe
Karen Russeth <kvrusseth@outlook.com>
2023-04-27 19:32:34 -0400

    JS Loopin Exercise 4-6 changes

--- week0_HTML_CSS/javascript-intro/script4.js
+++ week0_HTML_CSS/javascript-intro/script4.js
@@ -1,7 +1,36 @@
-let programming = {
-    languages: ["JavaScript", "Python", "Ruby"],
-    isChallenging: true,
-    isRewarding: true,
-    difficulty: 8,
-    jokes: "https://bit.ly/2ysFran"
-  };
+// Using a for loop console.log all the numbers from 0 to 10
+// Using a while loop console.log all the numbers from 0 to 10
+
+console.log("Numbers from 0 to 10");
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+
+//Even numbers to 20
+//Using a for loop console.log all the even numbers from 0 to 20
+
+console.log("Even numbers from 0 to 20");
+for (let j = 0; j <= 20; j+=2) {
+  console.log(j);
+}
+
+
+//Odd numbers to 20
+//Using a while loop console.log all the odd numbers from 0 to 20
+
+console.log("Odd umbers from 0 to 20");
+let k = 1;
+while (k <= 20) {
+  console.log(k);
+  k+=2;
+}
+
+
+//Back to zero
+//Using a for loop console.log all the numbers from 10 to 0.
+
+console.log("All numbers from 10 to 0");
+for (let num = 10; num >= 0; num--) {
+  console.log(num);
+}
