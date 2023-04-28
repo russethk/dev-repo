4377a74703a750af79f59f88adc909b327423f07
Karen Russeth <kvrusseth@outlook.com>
2023-04-28 11:13:04 -0400

    Food order function

--- week0_HTML_CSS/javascript-intro/script5.js
+++ week0_HTML_CSS/javascript-intro/script5.js
@@ -1,11 +1,11 @@
-
-
 function order(food) {
-  let food=prompt("Please enter what food you want");
+  food = prompt("Hi there!, What is your order?");
   return `I'll have the ${food}, please.`;
 }
 
-order("salad"); // "I'll have the salad, please."
-order("pizza"); // "I'll have the pizza, please."
-order("tacos"); // "I'll have the tacos, please."
+
+//order("salad"); // "I'll have the salad, please."
+//order("pizza"); // "I'll have the pizza, please."
+//order("tacos"); // "I'll have the tacos, please."
 //food is a parameter to our function!
+
