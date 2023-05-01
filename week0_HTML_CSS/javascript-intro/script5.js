20fedd9d1b90625031ffc97fe42ab651182269fb
Karen Russeth <kvrusseth@outlook.com>
2023-05-01 15:17:12 -0400

    sync js changes

--- week0_HTML_CSS/javascript-intro/script5.js
+++ week0_HTML_CSS/javascript-intro/script5.js
@@ -1,3 +1,8 @@
+// Function with an input prompt 
+// Calling the function creates a popup input box for the user.   
+// When usewr inputs response, return the value of the input(food) as an argument to the food parameter in the order function,
+// and then console.log a string with the user response ${food} included.  
+
 function order(food) {
   food = prompt("Hi there!, What is your order?");
   return `I'll have the ${food}, please.`;
