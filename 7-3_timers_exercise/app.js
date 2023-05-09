63c5a4e09f60c85a5ae72d212d17b3b6f1b4084c
Karen Russeth <kvrusseth@outlook.com>
2023-05-08 23:12:53 -0400

    Add 7-3_timers_exercise

---
+++ 7-3_timers_exercise/app.js
@@ -0,0 +1,29 @@
+
+function countdown(num){
+    let timer = setInterval(function() {
+        num--;
+        if(num <= 0) {
+            clearInterval(timer);
+            console.log('DONE!');
+        } else {
+            console.log(num);
+        }
+    },1000)  
+}
+
+
+function randomGame() {
+    let num;
+    let countTimes = 0;
+    let timer = setInterval(function() {
+        num = (Math.random() * 100 / 100).toFixed(2);
+        console.log(num);
+        countTimes++;
+        if(num > .75) {
+            clearInterval(timer);
+            console.log(`It took ${countTimes} try/tries to find a number greater than .75.`);
+        }
+    },1000) 
+}
+
+
