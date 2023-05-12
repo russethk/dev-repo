b6561b5bf5c39d365e191d3b1789e822430f1a38
Karen Russeth <kvrusseth@outlook.com>
2023-05-11 23:27:42 -0400

    commit 8.2.17 dom finding exercise

---
+++ 8-2-17_dom_finding/app.js
@@ -0,0 +1,35 @@
+document.getElementById("container");
+
+document.querySelector('section#container');
+
+document.getElementsByClassName('second');
+
+document.querySelector('ol .third');
+
+let sectionText = document.querySelector("#container");
+sectionText.innerText = "Hello!"
+
+let footer = document.querySelector('.footer');
+footer.classList.add('main');
+
+let footer = document.querySelector('.footer');
+footer.classList.remove('main');
+
+
+let newLi = document.createElement("li");
+newLi.innerText = 'four';
+
+let list = document.querySelector("ul");
+list.appendChild(newLi);
+
+let liInsideOl = document.querySelectorAll('ol li');
+
+for (let i = 0; i < liInsideOl.length; i++) {
+    liInsideOl[i].style.backgroundColor = 'green';
+}
+
+let footer = document.querySelector('.footer');
+footer.remove();
+
+
+
