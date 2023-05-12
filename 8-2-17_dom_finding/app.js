393e35d6395060646b11aa60dcb8afdbdd1fd159
Karen Russeth <kvrusseth@outlook.com>
2023-05-12 07:35:33 -0400

    comment out js code

--- 8-2-17_dom_finding/app.js
+++ 8-2-17_dom_finding/app.js
@@ -1,19 +1,19 @@
-document.getElementById("container");
-
-document.querySelector('section#container');
-
-document.getElementsByClassName('second');
-
-document.querySelector('ol .third');
-
-let sectionText = document.querySelector("#container");
-sectionText.innerText = "Hello!"
-
-let footer = document.querySelector('.footer');
-footer.classList.add('main');
-
-let footer = document.querySelector('.footer');
-footer.classList.remove('main');
+// document.getElementById("container");
+// 
+// document.querySelector('section#container');
+// 
+// document.getElementsByClassName('second');
+// 
+// document.querySelector('ol .third');
+// 
+// let sectionText = document.querySelector("#container");
+// sectionText.innerText = "Hello!"
+// 
+// let footer = document.querySelector('.footer');
+// footer.classList.add('main');
+// 
+// let footer = document.querySelector('.footer');
+// footer.classList.remove('main');
 
 
 let newLi = document.createElement("li");
@@ -28,8 +28,8 @@ for (let i = 0; i < liInsideOl.length; i++) {
     liInsideOl[i].style.backgroundColor = 'green';
 }
 
-let footer = document.querySelector('.footer');
-footer.remove();
+// let footer = document.querySelector('.footer');
+// footer.remove();
 
 
 
