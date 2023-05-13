e779ef972234d0a39a0c2ef777393eb28d463844
Karen Russeth <kvrusseth@outlook.com>
2023-05-13 18:45:10 -0400

    todo list fixes

---
+++ 8-4-6_todo-list/form-localStorage.js
@@ -0,0 +1,44 @@
+const todoForm = document.querySelector("form");
+const todoList = document.querySelector("#todo-list");
+
+// retrieve from localStorage
+const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+for (let i = 0; i < savedTodos.length; i++) {
+  let newTodo = document.createElement("li");
+  newTodo.innerText = savedTodos[i].task;
+  newTodo.isCompleted = savedTodos[i].isCompleted ? true : false;
+  if (newTodo.isCompleted) {
+    newTodo.style.textDecoration = "line-through";
+  }
+  todoList.appendChild(newTodo);
+}
+
+todoForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  let newTodo = document.createElement("li");
+  let taskValue = document.getElementById("task").value;
+  newTodo.innerText = taskValue;
+  newTodo.isCompleted = false;
+  todoForm.reset();
+  todoList.appendChild(newTodo);
+
+  // save to localStorage
+  for (let i = 0; i < savedTodos.length; i++) {
+    if (savedTodos[i].task === clickedListItem.innerText) {
+      savedTodos[i].isCompleted = !savedTodos[i].isCompleted;
+      localStorage.setItem("todos", JSON.stringify(savedTodos));
+    }
+  }
+});
+
+todoList.addEventListener("click", function(event) {
+  let clickedListItem = event.target;
+
+  if (!clickedListItem.isCompleted) {
+    clickedListItem.style.textDecoration = "line-through";
+    clickedListItem.isCompleted = true;
+  } else {
+    clickedListItem.style.textDecoration = "none";
+    clickedListItem.isCompleted = false;
+  }
+});
