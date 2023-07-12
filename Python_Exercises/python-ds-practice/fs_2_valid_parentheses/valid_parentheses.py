2950f49406f3ea6124561fd5cbe05d4873ba8b64
Karen Russeth <kvrusseth@gmail.com>
2023-07-12 08:57:27 -0400

    push updated file valid_parentheses.py

--- Python_Exercises/python-ds-practice/fs_2_valid_parentheses/valid_parentheses.py
+++ Python_Exercises/python-ds-practice/fs_2_valid_parentheses/valid_parentheses.py
@@ -22,3 +22,17 @@ def valid_parentheses(parens):
         >>> valid_parentheses(")()(")
         False
     """
+    
+    count = 0
+
+    for char in parens:
+        if char == '(':
+            count += 1
+        elif char == ')':
+            count -= 1
+
+        # fast fail if too many right parens
+        if count < 0:
+            return False
+        
+    return count == 0
