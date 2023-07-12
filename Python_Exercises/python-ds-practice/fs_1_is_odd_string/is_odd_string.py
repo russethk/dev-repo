ee91868871df296bf1f386ae9ed1c07206423e3c
Karen Russeth <kvrusseth@gmail.com>
2023-07-12 08:02:34 -0400

    push updated files

--- Python_Exercises/python-ds-practice/fs_1_is_odd_string/is_odd_string.py
+++ Python_Exercises/python-ds-practice/fs_1_is_odd_string/is_odd_string.py
@@ -28,4 +28,18 @@ def is_odd_string(word):
         True
     """
 
-    # Hint: you may find the ord() function useful here
+    # use the ord() function to find the unicaode integer value of the character
+    # subtract 1 to get the actual position of the first character (first char is always 0)
+    # loop through the string of characters, adding the value of the integer values of the chars
+    # assign the sum of those values to total variable
+    # retrun true or false if the value of total % 2 == 1 (is odd) 
+
+    DIFF = ord("a") - 1
+    total = sum((ord(c) - DIFF) for c in word.lower())
+    return total % 2 == 1
+
+        
+
+    
+    
+    
