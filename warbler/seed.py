1ffe8b4c2c251d109dcc41975f2bca98944cc942
Karen Russeth <kvrusseth@gmail.com>
2023-09-13 14:03:54 -0400

    push updates

--- warbler/seed.py
+++ warbler/seed.py
@@ -1,11 +1,12 @@
 """Seed database with sample data from CSV Files."""
 
 from csv import DictReader
-from app import db
+from app import app, db
 from models import User, Message, Follows
 
-
-db.drop_all()
+# Create all tables
+app.app_context().push()
+db.create_all()
 
 with open('generator/users.csv') as users:
     db.session.bulk_insert_mappings(User, DictReader(users))
