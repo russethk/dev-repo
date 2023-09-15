e27c02304f1e359441dfb5a4060c4bb97e0b3e6d
Karen Russeth <kvrusseth@gmail.com>
2023-09-15 14:03:28 -0400

    updates

--- warbler/app.py
+++ warbler/app.py
@@ -13,8 +13,7 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
+app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL'))
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
