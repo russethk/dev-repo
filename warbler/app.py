ad9271f15cce3eb28fe8c2a0cb6ee9fed1494a00
Karen Russeth <kvrusseth@gmail.com>
2023-09-15 17:41:56 -0400

    updates

--- warbler/app.py
+++ warbler/app.py
@@ -5,6 +5,8 @@ from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
 from dotenv import load_dotenv
+load_dotenv()
+
 
 
 from forms import UserAddForm, LoginForm, MessageForm
@@ -25,7 +27,6 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
-load_dotenv()
 
 
 
