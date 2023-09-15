5b643ec07e7152cd13236c9514e87878f5adceea
Karen Russeth <kvrusseth@gmail.com>
2023-09-15 18:08:38 -0400

    updates

--- warbler/app.py
+++ warbler/app.py
@@ -4,9 +4,6 @@ from flask import Flask, render_template, request, flash, redirect, session, g
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
-from dotenv import load_dotenv
-
-
 from forms import UserAddForm, LoginForm, MessageForm
 from models import db, connect_db, User, Message
 
@@ -14,12 +11,9 @@ CURR_USER_KEY = "curr_user"
 
 app = Flask(__name__)
 
-load_dotenv()
-
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
 app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
