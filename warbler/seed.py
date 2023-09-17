a5e736585acb3086d830b676bc1135187ac10ae7
Karen Russeth <kvrusseth@gmail.com>
2023-09-17 19:31:32 -0400

    updates

--- warbler/seed.py
+++ warbler/seed.py
@@ -4,9 +4,8 @@ from csv import DictReader
 from app import db
 from models import User, Message, Follows
 
-
-db.drop_all()
 db.create_all()
+
 
 with open('generator/users.csv') as users:
     db.session.bulk_insert_mappings(User, DictReader(users))
