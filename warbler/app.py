9ddd729cd343e9c8edfb84c10ad08ca704cd0e95
Karen Russeth <kvrusseth@gmail.com>
2023-09-17 13:59:12 -0400

    implement likes feature on home page

--- warbler/app.py
+++ warbler/app.py
@@ -1,7 +1,7 @@
 import os
 import pdb
 
-from flask import Flask, render_template, request, flash, redirect, session, g
+from flask import Flask, render_template, request, flash, redirect, session, g, abort
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
@@ -207,7 +207,40 @@ def stop_following(follow_id):
 
     return redirect(f"/users/{g.user.id}/following")
 
+app.route('/users/<int:user_id>/likes', methods=["GET"])
+def show_likes(user_id):
+    """Show list of messages this user has liked."""
 
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+    
+    user = User.query.get_or_404(user_id)
+    return render_template('users/likes.html', user=user, likes=user.likes)
+
+app.route("/messages/<int:message_id>/like", methods=["POST"])
+def add_like(message_id):
+    """Add a like for the currently-logged-in user."""
+
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+    
+    liked_message = Message.query.get_or_404(message_id)
+    if liked_message.user_id == g.user.id:
+        return abort(403)
+    
+    user_likes = g.user.likes
+
+    if liked_message in user_likes:
+        g.user.likes = [like for like in user_likes if like != liked_message]
+    else:
+        g.user.likes.append(liked_message)
+
+    db.session.commit()
+
+    return redirect("/")
+    
 @app.route('/users/profile', methods=["GET", "POST"])
 def edit_profile():
     """Update profile for current user."""
@@ -232,7 +265,7 @@ def edit_profile():
 
         flash("Wrong password, please try again.", 'danger')
 
-    return render_template('users/edit.html', form=form, user_id=user.id)
+    return render_template('users/edit.html', form=form, user_id=user.id, user=user)
 
 
 
@@ -299,6 +332,8 @@ def messages_destroy(message_id):
     db.session.commit()
 
     return redirect(f"/users/{g.user.id}")
+
+
 
 
 ##############################################################################
