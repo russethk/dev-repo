d7ca2c0f3809630c5a7f49019bfe5a10db0ec56e
Karen Russeth <kvrusseth@gmail.com>
2023-09-25 17:10:57 -0400

    add webCat function to step2.js in node-files

--- node-files/step2.js
+++ node-files/step2.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const process = require('process');
+const axios = require('axios');
 
 // read file with given path and print contents
 
@@ -17,4 +18,26 @@ function cat(path) {
   });
 }
 
-cat(process.argv[2]);
+/** read page at URL and print it out. */
+
+async function webCat(url) {
+  try {
+    let resp = await axios.get(url);
+    console.log(resp.data);
+  } catch (err) {
+    console.error(`Error fetching ${url}: ${err}`);
+    process.exit(1);
+  }
+}
+
+let path = process.argv[2];
+
+if (path.slice(0, 4) === 'http') {
+  webCat(path);
+} else {
+  cat(path);
+}
+
+
+
+
