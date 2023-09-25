82eb2263d79946ef3c8b74f3b83969930ccb27a8
Karen Russeth <kvrusseth@gmail.com>
2023-09-25 17:44:36 -0400

    add output to file function to step3.js

--- node-files/step3.js
+++ node-files/step3.js
@@ -2,9 +2,24 @@ const fs = require('fs');
 const process = require('process');
 const axios = require('axios');
 
+// --out output-filename.txt readfile-or-url
+
+function handleOutput(text, out) {
+  if (out) {
+    fs.writeFile(out, text, 'utf8', function(err) {
+      if (err) {
+        console.error(`Couldn't write ${out}: ${err}`);
+        process.exit(1);
+      }
+    });
+  } else {
+    console.log(text);
+  }
+}
+
 // read file with given path and print contents
 
-function cat(path) {
+function cat(path, out) {
   fs.readFile(path, 'utf8', (err, data) => {
     if (err) {
       // handle possible error
@@ -12,30 +27,31 @@ function cat(path) {
       // kill the process and tell the shell it errored
       process.exit(1);
     } else {
-      // otherwise success
-      console.log(data);
+      // call handleOutput to output the contents
+      handleOutput(data, out);
     }
   });
 }
 
 /** read page at URL and print it out. */
 
-async function webCat(url) {
+async function webCat(url, out) {
   try {
     let resp = await axios.get(url);
-    console.log(resp.data);
+    handleOutput(resp.data, out);
   } catch (err) {
     console.error(`Error fetching ${url}: ${err}`);
     process.exit(1);
   }
 }
 
-let path = process.argv[2];
+let path;
+let out;
 
 if (path.slice(0, 4) === 'http') {
-  webCat(path);
+  webCat(path, out);
 } else {
-  cat(path);
+  cat(path, out);
 }
 
 
