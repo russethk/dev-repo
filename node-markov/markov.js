0765f97bc48e2d2f0e9fddb39174888c0b34d34a
Karen Russeth <kvrusseth@gmail.com>
2023-09-27 19:30:04 -0400

    push markov machine to github

--- node-markov/markov.js
+++ node-markov/markov.js
@@ -17,13 +17,43 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    let chains = new Map();
+
+    for (let i = 0; i < this.words.length; i += 1) {
+      let word = this.words[i];
+      let nextWord = this.words[i + 1] || null;
+
+      if (chains.has(word)) chains.get(word).push(nextWord);
+      else chains.set(word, [nextWord]);
+    }
+
+    this.chains = chains;
+  }
+
+  /** Pick random choice from array */
+
+  static choice(ar) {
+    return ar[Math.floor(Math.random() * ar.length)];
   }
 
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
-    // TODO
+    // pick a random key to begin
+    let keys = Array.from(this.chains.keys());
+    let key = MarkovMachine.choice(keys);
+    let out = [];
+
+    // produce markov chain until reaching termination word
+    while (out.length < numWords && key !== null) {
+      out.push(key);
+      key = MarkovMachine.choice(this.chains.get(key));
+    }
   }
 }
+
+module.exports = {
+  MarkovMachine
+};
+
