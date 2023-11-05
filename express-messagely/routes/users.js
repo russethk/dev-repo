e0a64d723bc647f66f60d1d55c4bd637a4fd2bfa
Karen Russeth <kvrusseth@gmail.com>
2023-11-05 12:20:55 -0500

    added functioanlity to routes

--- express-messagely/routes/users.js
+++ express-messagely/routes/users.js
@@ -1,8 +1,24 @@
+const Router = require("express").Router;
+const User = require("../models/user");
+const {ensureLoggedIn, ensureCorrectUser} = require("../middleware/auth");
+const router = new Router();
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
+
+router.get("/", ensureLoggedIn, async function (req, res, next) {
+    try {
+        let users = await User.all();
+        return res.json({users});
+    }
+
+    catch (err) {
+        return next(err);
+    }
+});
 
 
 /** GET /:username - get detail of users.
@@ -10,6 +26,17 @@
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
+
+router.get("/:username", ensureCorrectUser, async function (req, res, next) {
+    try {
+        let user = await User.get(req.params.username);
+        return res.json({user});
+    }
+
+    catch (err) {
+        return next(err);
+    }
+});
 
 
 /** GET /:username/to - get messages to user
@@ -22,6 +49,17 @@
  *
  **/
 
+router.get("/:username/to", ensureCorrectUser, async function (req, res, next) {
+    try {
+        let messages = await User.messagesTo(req.params.username);
+        return res.json({messages});
+    }
+
+    catch (err) {
+        return next(err);
+    }
+});
+
 
 /** GET /:username/from - get messages from user
  *
@@ -32,3 +70,16 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+
+router.get("/:username/from", ensureCorrectUser, async function (req, res, next) {
+    try {
+        let messages = await User.messagesFrom(req.params.username);
+        return res.json({messages});
+    }
+
+    catch (err) {
+        return next(err);
+    }
+});
+
+module.exports = router;
