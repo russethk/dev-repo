b0f70278c5b23846599daee2bc48e4aea87fbe1c
Karen Russeth <kvrusseth@gmail.com>
2023-11-16 19:54:05 -0500

    update company model to include jobs query

--- express-jobly/models/company.js
+++ express-jobly/models/company.js
@@ -104,7 +104,7 @@ class Company {
   /** Given a company handle, return data about company.
    *
    * Returns { handle, name, description, numEmployees, logoUrl, jobs }
-   *   where jobs is [{ id, title, salary, equity, companyHandle }, ...]
+   *   where jobs is [{ id, title, salary, equity }, ...]
    *
    * Throws NotFoundError if not found.
    **/
@@ -123,6 +123,16 @@ class Company {
     const company = companyRes.rows[0];
 
     if (!company) throw new NotFoundError(`No company: ${handle}`);
+
+    const jobsRes = await db.query(
+          `SELECT id, title, salary, equity
+           FROM jobs
+           WHERE company_handle = $1
+           ORDER BY id`,
+        [handle],
+    );
+
+    company.jobs = jobsRes.rows;
 
     return company;
   }
