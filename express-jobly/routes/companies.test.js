229b2687cbdf47871739271771fb9cc5faaaf5e1
Karen Russeth <kvrusseth@gmail.com>
2023-11-19 08:28:13 -0500

    add test for jobs to companies.test.js

--- express-jobly/routes/companies.test.js
+++ express-jobly/routes/companies.test.js
@@ -189,6 +189,11 @@ describe("GET /companies/:handle", function () {
         description: "Desc1",
         numEmployees: 1,
         logoUrl: "http://c1.img",
+        jobs: [
+          { id: testJobIds[0], title: "j1", equity: "0.1", salary: 1 },
+          { id: testJobIds[1], title: "j2", equity: "0.2", salary: 2 },
+          { id: testJobIds[2], title: "j3", equity: null, salary: 3 },
+        ],
       },
     });
   });
@@ -202,6 +207,7 @@ describe("GET /companies/:handle", function () {
         description: "Desc2",
         numEmployees: 2,
         logoUrl: "http://c2.img",
+        jobs: [],
       },
     });
   });
