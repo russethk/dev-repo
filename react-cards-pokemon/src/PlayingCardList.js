2d531bda850cded88cebf15b2cdcb86e6bfcab74
Karen Russeth <kvrusseth@gmail.com>
2023-12-27 15:40:06 -0500

    Part 3 - Created and added Custom hook useAxios for API calls

--- react-cards-pokemon/src/PlayingCardList.js
+++ react-cards-pokemon/src/PlayingCardList.js
@@ -1,28 +1,26 @@
-import React, { useState } from "react";
-import {v4 as uuid} from "uuid";
-import axios from "axios";
+import React from "react";
+import {useAxios } from "./hooks";
 import PlayingCard from "./PlayingCard";
+import { formatCard } from "./helpers";
 import "./PlayingCardList.css";
 
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function CardTable() {
-  const [cards, setCards] = useState([]);
-  const addCard = async () => {
-    const response = await axios.get(
-      "https://deckofcardsapi.com/api/deck/new/draw/"
-    );
-    setCards(cards => [...cards, { ...response.data, id: uuid() }]);
-  };
+  // Refactored to use custom hook useAxios
+  const [cards, addCard] = useAxios(
+    "cards",
+    "https://deckofcardsapi.com/api/deck/new/draw/"
+  );
   return (
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
       <div>
-        <button onClick={addCard}>Add a playing card!</button>
+        <button onClick={() => addCard(formatCard)}>Add a playing card!</button>
       </div>
       <div className="PlayingCardList-card-area">
-        {cards.map(cardData => (
-          <PlayingCard key={cardData.id} front={cardData.cards[0].image} />
+        {cards.map(card => (
+          <PlayingCard key={card.id} front={card.image} />
         ))}
       </div>
     </div>
--- react-cards-pokemon/src/hooks.js
+++ react-cards-pokemon/src/hooks.js
@@ -12,5 +12,51 @@ function useFlip(initialFlipState = true) {
   return [isFlipped, flip];
 }
 
+// Part 3 - Custom hook for API calls
+/* useAxios takes in a URL, and returns an array with two elements. 
+The first element is an array of data obtained from previous AJAX requests
+The second element is a function that will add a new object of data to the array.
+The responses array is stored in localStorage under the provided key.
+*
+* The function addResponseData takes in two optional parameters:
+* formatter: a function that will be applied to the response data before it is added to the array
+* restOfUrl: a string that will be appended to the baseUrl when making the AJAX request
+*
+* The function clearResponses will empty the array of data.
+*/
 
-export { useFlip };
+function useAxios(keyInLS, baseUrl) {
+  const [responses, setResponses] = useLocalStorage(keyInLS);
+
+  const addResponseData = async (formatter = data => data, restOfUrl = '') => {
+    const response = await axios.get(`${baseUrl}${restOfUrl}`);
+    setResponses(data => [...data, formatter(response.data)]);
+  };
+
+  const clearResponses = () => setResponses([]);
+
+  return [responses, addResponseData, clearResponses];
+  
+}
+
+// Further Study - Custom hook for local storage
+/* useLocalStorage takes in a key and an initial value, and returns an array with two elements.
+The first element is the current value stored in localStorage under the provided key.
+The second element is a function to update that value in localStorage.
+*/
+function useLocalStorage(key, initialValue = []) {
+  if (localStorage.getItem(key)) {
+    initialValue = JSON.parse(localStorage.getItem(key));
+  }
+  const [value, setValue] = useState(initialValue);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue];
+}
+
+export default useLocalStorage;
+
+export { useFlip, useAxios, useLocalStorage };
