4da05a1bb5ad7963a63d643b2a04e1bb90260007
Karen Russeth <kvrusseth@gmail.com>
2024-01-05 20:28:17 -0500

    added button and counter to Soda.js and Candy.js

--- vending-machine/src/Candy.js
+++ vending-machine/src/Candy.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link }from 'react-router-dom';
 import candyImage from "./img/Nerds-Gummy.png";
+import './Candy.css';
 
-function Candy() {
-    return (
-        <div className="Candy">
-        <h1>Candy</h1>
-        <img src={candyImage} alt="Nerds Gummy Rainbow Clusters"/>
-        </div>
-    );
+
+class Candy extends Component {
+   constructor(props) {
+        super(props);
+        this.state = { 
+            candies: [],
+        };
+        this.handleClick = this.handleClick.bind(this);
+     }
+    
+    handleClick() {
+        const x = window.innerWidth * Math.random();
+        const y = window.innerHeight * Math.random();
+        this.setState(prevState => ({
+            candies: [...prevState.candies, {x, y}]
+        }));
+    }
+
+    render() {
+        const bags = this.state.candies.map((candy, i) => (
+            <img 
+                key={i} 
+                src={candyImage} 
+                className="candy" 
+                style={{top: `${candy.y}px`, left: `${candy.x}px`}}
+                alt="nerds candy"
+            />
+        ));
+        return (
+            <div className="Candy">
+                <h1>candies eaten: {this.state.candies.length}</h1>
+                <button onClick={this.handleClick}>NOM NOM NOM</button>
+                <Link to="/">Go Back</Link>
+                {bags}
+            </div>
+        );
+    }
 }
-
 
 export default Candy;
--- vending-machine/src/Soda.js
+++ vending-machine/src/Soda.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link }from 'react-router-dom';
 import sodaImage from "./img/Coca-Cola-Cherry.png";
+import './Soda.css';
 
 
-function Soda() {
-    return (
-        <div className="Chips">
-        <h1>Soda</h1>
-        <img src={sodaImage} alt="Coca-Cola Cherry Zero 12 oz Soda"/>
-        </div>
-    );
+class Soda extends Component {
+   constructor(props) {
+        super(props);
+        this.state = { 
+            bottles: [],
+        };
+        this.handleClick = this.handleClick.bind(this);
+     }
+    
+    handleClick() {
+        const x = window.innerWidth * Math.random();
+        const y = window.innerHeight * Math.random();
+        this.setState(prevState => ({
+            bottles: [...prevState.bottles, {x, y}]
+        }));
+    }
+
+    render() {
+        const bags = this.state.bottles.map((bottle, i) => (
+            <img 
+                key={i} 
+                src={sodaImage} 
+                className="bottle" 
+                style={{top: `${bottle.y}px`, left: `${bottle.x}px`}}
+                alt="bottle of soda"
+            />
+        ));
+        return (
+            <div className="Soda">
+                <h1>sodas drank: {this.state.bottles.length}</h1>
+                <button onClick={this.handleClick}>aaahhhh....</button>
+                <Link to="/">Go Back</Link>
+                {bags}
+            </div>
+        );
+    }
 }
-
 
 export default Soda;
