a4fe6e71cc9bd7dab2785f35de18d3e327072c88
Karen Russeth <kvrusseth@gmail.com>
2024-01-05 20:03:59 -0500

    Add chips component with button and counter

--- vending-machine/src/Chips.js
+++ vending-machine/src/Chips.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link }from 'react-router-dom';
 import chipsImage from "./img/Doritos-Nacho-Cheese.png";
+import './Chips.css';
 
 
-function Chips() {
-    return (
-        <div className="Chips">
-        <h1>Chips</h1>
-        <img src={chipsImage} alt="Doritos nacho cheese chips"/>
-        </div>
-    );
+class Chips extends Component {
+   constructor(props) {
+        super(props);
+        this.state = { 
+            bags: [],
+        };
+        this.handleClick = this.handleClick.bind(this);
+     }
+    
+    handleClick() {
+        const x = window.innerWidth * Math.random();
+        const y = window.innerHeight * Math.random();
+        this.setState(prevState => ({
+            bags: [...prevState.bags, {x, y}]
+        }));
     }
 
+    render() {
+        const bags = this.state.bags.map((bag, i) => (
+            <img 
+                key={i} 
+                src={chipsImage} 
+                className="bag" 
+                style={{top: `${bag.y}px`, left: `${bag.x}px`}}
+                alt="bag of chips"
+            />
+        ));
+        return (
+            <div className="Chips">
+                <h1>bags eaten: {this.state.bags.length}</h1>
+                <button onClick={this.handleClick}>nom nom nom</button>
+                <Link to="/">Go Back</Link>
+                {bags}
+            </div>
+        );
+    }
+}
 
 export default Chips;
